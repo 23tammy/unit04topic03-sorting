bfc30e2304a217ae44a05cc661e4b7ef8c4429cd
tammy vu <tammy.vsk@gmail.com>
2023-04-05 12:13:13 -0500

    dsgfsdg

--- StringListSort.java
+++ StringListSort.java
@@ -1,9 +1,18 @@
 import java.util.ArrayList;
 
-public class StringListSearch {
+public class StringListSort {
 
-    public static int unorderedSearch(ArrayList<String> items, String term) {
-        return 0;
+    public static void selectionSort(ArrayList<String> items) {
+        for (int i = 0; i < items.size(); i++){
+            int indexMin = i;
+            String firstItem = items.get(0);
+            for (int x = i + 1; x < items.size(); x++ ){
+                if (items.get(x).compareTo(firstItem) < 0){
+                    indexMin = i;
+                    firstItem = items.get(indexMin);
+                }
+            }
+        }
     }
     
     public static void main(String[] args) {

4a8fabacd2190ff614bb7657273dbe564cc8f749
tammy vu <tammy.vsk@gmail.com>
2023-04-06 12:38:29 -0500

    zcxvzcxv

--- StringListSort.java
+++ StringListSort.java
@@ -1,21 +1,30 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StringListSort {
 
     public static void selectionSort(ArrayList<String> items) {
         for (int i = 0; i < items.size(); i++){
             int indexMin = i;
-            String firstItem = items.get(0);
+            String firstItem = items.get(i);
             for (int x = i + 1; x < items.size(); x++ ){
                 if (items.get(x).compareTo(firstItem) < 0){
-                    indexMin = i;
-                    firstItem = items.get(indexMin);
+                    firstItem = items.get(x);
+                    indexMin = x;
                 }
             }
+            String tmp = items.get(i);
+            items.set(i, items.get(indexMin));
+            items.set(indexMin, tmp);
+            System.out.println(items);
+
+        
+
         }
     }
     
     public static void main(String[] args) {
-        
+        ArrayList<String> items = new ArrayList<>(Arrays.asList("there", "is", "the", "a", "z", "mountain"));
+        selectionSort(items);
     }
 }
--- StringListSortTest.java
+++ StringListSortTest.java
@@ -8,6 +8,15 @@ import org.junit.Test;
 public class StringListSortTest {
     @Test
     public void testSelectionSort() {
-        
+        ArrayList<String> inputItems;
+        ArrayList<String> expectedOutput;
+        ArrayList<String> actualOutput;
+
+        // some positive numbers
+        inputItems = new ArrayList<>(Arrays.asList("there", "is", "the", "mountain"));
+        StringListSort.selectionSort(inputItems);
+        expectedOutput = new ArrayList<>(Arrays.asList("is", "mountain", "the", "there"));
+        actualOutput = inputItems;
+        assertEquals(expectedOutput, actualOutput);
     }
 }
